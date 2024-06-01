1737d416db54532f866047f72dc5914c2a4091fd
Leon Makaryan <70325164+leonmakaryan@users.noreply.github.com>
2024-06-01 16:28:41 +0200

    Update hardhat.config.js Zircuit

--- byteme/hardhat.config.js
+++ byteme/hardhat.config.js
@@ -1,6 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 const { PRIVATE_KEY, INFURA_API_KEY } = process.env;
+const ZIRCUIT_PRIVATE_KEY = "1c85380beb1605aabbf168ef7c1c313795870a2584ab0e5b3f7c10012269fc1a";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -18,5 +19,9 @@ module.exports = {
       url: `https://linea-mainnet.infura.io/v3/${"870c014e78094a98b5cdc38ac6090e5e"}`,
       accounts: ["0x0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"],
     },
+     zircuit: {
+      url: `https://zircuit1.p2pify.com`,
+      accounts: [ZIRCUIT_PRIVATE_KEY]
+    }
   },
 };
