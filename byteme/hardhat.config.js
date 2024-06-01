528e95184e841218139b1316e686550c8d3b932c
Mikhail Azaryan <mikhail.azaryan@tum.de>
2024-06-01 16:14:35 +0200

    connect linea

--- byteme/hardhat.config.js
+++ byteme/hardhat.config.js
@@ -7,16 +7,16 @@ module.exports = {
   solidity: "0.8.24",
   networks: {
     linea_goerli: {
-      url: `https://linea-goerli.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [PRIVATE_KEY],
+      url: `https://linea-goerli.infura.io/v3/${"870c014e78094a98b5cdc38ac6090e5e"}`,
+      accounts: ["0x0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"],
     },
     linea_sepolia: {
-      url: `https://linea-sepolia.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [PRIVATE_KEY],
+      url: `https://linea-sepolia.infura.io/v3/${"0x870c014e78094a98b5cdc38ac6090e5e"}`,
+      accounts: ["0x0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"],
     },
     linea_mainnet: {
-      url: `https://linea-mainnet.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [PRIVATE_KEY],
+      url: `https://linea-mainnet.infura.io/v3/${"870c014e78094a98b5cdc38ac6090e5e"}`,
+      accounts: ["0x0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"],
     },
   },
 };
